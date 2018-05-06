db3f9bb229d0adee91e6b23e1a0206840ca8fbf0
eric <ericalvernhe@orange.fr>
2018-05-06 22:39:12 +0200

    style(build): format config files

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,40 +2,41 @@ const PrettierPlugin = require("prettier-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.js"
-    ,path: __dirname + "/dist"
-  },
-  devtool: "source-map",
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  module: {
-    rules: [
-       {
-          test: /\.tsx?$/,
-          loader: "awesome-typescript-loader"
-        },
-        { 
-          test: /\.js$/, 
-          enforce: "pre", 
-          loader: "source-map-loader" },
-        {
-          test: /\.html$/,
-          use: [
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
             {
-              loader: "html-loader",
-              options: { minimize: true }
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: {
+                        minimize: true
+                    }
+                }]
             }
-          ]
-        }
+        ]
+    },
+    plugins: [
+        new PrettierPlugin(), 
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
     ]
-  },
-  plugins: [  new PrettierPlugin()
-, new HtmlWebPackPlugin({ 
-    template: "./src/index.html",
-    filename: "./index.html"
-})]
 };
